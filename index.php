b990f73de83e354b63f50ef3796af88ceaa16990
dalton <dalton.polhill@tulip.io>
2017-06-20 08:14:12 -0400

    followed the w3 tutorial to build a website on index.php, cannot get the css styles to work properly

--- index.php
+++ index.php
@@ -1,16 +1,52 @@
 <HTML>
     <HEAD>
-        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" href="style.css">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
     </HEAD>
 
     <title>Welcome</title>
 
     <body>
 
-        <div>
-            <p>This is the homepage of the website, here will be some information on the site
-                this will have a sign in and register button for users</p>
-        </div>
+        <nav class="headerNavBar">
+            <a href="#home">Home</a>
+            <a href="#band">Band</a>
+            <a href="#tour">Tour</a>
+            <a href="#contact">Contact</a>
+        </nav>
+
+        <section>
+            <img src="images/img1.jpg" style="width:50%">
+        </section>
+
+        <section style="max-width:600px">
+            <h2>THE BAND</h2>
+            <p><i>We love music</i></p>
+        </section>
+
+        <section style="max-width:600px">
+            <p>We have created a fictional band website. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </section>
+
+        <section >
+            <article>
+                <p>John</p>
+            </article>
+            <article >
+                <p>Paul</p>
+            </article>
+            <article >
+                <p>Ringo</p>
+            </article>
+        </section>
+
+        <footer>
+            <a href="#facebook"> <i> Facebook</i> </a>
+            <a href="#twitter"> <i>Twitter</i> </a>
+            <a href="#instagram"> <i>Instagram</i> </a>
+            <p> <i> Powered by </i> <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a> </p>
+        </footer>
 
         <form method="post" action="login.php">
             <button type="submit" id="button">Sign In / Register</button>
@@ -18,7 +54,4 @@
     </body>
 
 </HTML>
-
-
-
 
