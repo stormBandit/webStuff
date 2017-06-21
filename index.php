d79b6b14933d11be5acb11c2d110428554acadb0
dalton <dalton.polhill@tulip.io>
2017-06-21 17:08:41 -0400

    added in the gunctionality for the pure css lightbox

--- index.php
+++ index.php
@@ -18,10 +18,6 @@
             </form>
         </nav>
 
-        <section>
-            <img src="images/img1.jpg" style="width:50%">
-        </section>
-
         <section style="max-width:600px">
             <h2>THE BAND</h2>
             <p><i>We love music</i></p>
@@ -31,16 +27,33 @@
             <p>We have created a fictional band website. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         </section>
 
+        <p>Here are some 'lightbox' examples [ <a href="#example1">Open example #1</a> | <a href="#example2">Open example #2</a> ]</p>
+
+        <div class="lightbox" id="example1">
+            <figure>
+                <a href="#" class="closeButton"></a>
+                <figcaption>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec felis enim, placerat id eleifend eu, semper vel sem. Sed interdum commodo enim venenatis pulvinar. Proin mattis lorem vitae diam scelerisque hendrerit. Fusce cursus imperdiet mauris, vitae hendrerit velit dignissim a. Suspendisse potenti. Aenean feugiat facilisis diam, in posuere sapien mattis vel. Proin molestie rutrum diam, pharetra feugiat ligula sollicitudin sed. Etiam cursus diam quis tellus aliquam gravida. Aliquam erat volutpat.<br />
+                    Etiam varius adipiscing mi eget imperdiet. Nulla quis vestibulum leo. Integer molestie massa ut massa commodo in blandit purus aliquam. Mauris sit amet posuere massa. Ut a eleifend augue. Proin sodales mauris nec tellus pharetra dictum.</figcaption>
+            </figure>
+        </div>
+
+        <div class="lightbox" id="example2">
+            <figure>
+                <a href="#" class="closeButton"></a>
+                <figcaption>Cras risus odio, pharetra nec ultricies et, mollis ac augue. Nunc et diam quis sapien dignissim auctor. Quisque quis neque arcu, nec gravida magna. Etiam ullamcorper augue quis orci posuere et tincidunt augue semper. Maecenas varius augue eu orci auctor bibendum tristique ligula egestas. Morbi pharetra tortor iaculis erat porta id aliquam leo cursus. Ut nec elit vel mauris dapibus lacinia eget sed odio.</figcaption>
+            </figure>
+        </div>
+
+        <form method="post" action="login.php">
+            <button type="submit" class="mainButton">Sign In / Register</button>
+        </form>
+
         <footer>
             <a href="#facebook"> <i> Facebook</i> </a>
             <a href="#twitter"> <i>Twitter</i> </a>
             <a href="#instagram"> <i>Instagram</i> </a>
             <p> <i> Powered by </i> <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a> </p>
         </footer>
-
-        <form method="post" action="login.php">
-            <button type="submit" class="mainButton">Sign In / Register</button>
-        </form>
     </body>
 
 </HTML>
