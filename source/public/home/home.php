11fe64e34f78f02fc78da5db358839acfd395f79
stormBandit <dalton.polhill@adknown.com>
2019-04-11 11:49:05 -0400

    working on the experience section

--- source/public/home/home.php
+++ source/public/home/home.php
@@ -12,14 +12,13 @@ require_once (__DIR__ . "/../header.html");
     <div id="profileSection" class="container-fluid section profile-section">
         <div class="content">
             <h1>Profile</h1>
-<!--            <img class="profile-picture" src="/../../global/images/me.jpg" alt="" height="188" width="188">-->
             <p class="lead-in">Computer Science Major - Software Engineer</p>
             <hr>
             <div class="row">
                 <div class="col-md-5">
                     <h3>Who I Am</h3>
                     <p>
-                        I am a fourth year coop student at the he University of Guelph, pursuing a Major in Computer Science
+                        I am a fourth year coop student at the University of Guelph, pursuing a Major in Computer Science
                         and a minor in Business. I have experience designing, implementing and testing various systems across
                         web and mobile platforms. I have spent four years developing my skill as a software engineer and
                         becoming familiar with a variety of tools and languages popular in the industry. I have had experience
@@ -34,8 +33,8 @@ require_once (__DIR__ . "/../header.html");
                 <div class="col-md-3">
                     <h3>Details</h3>
                     <p>
-                        <b>Expected Graduation:</b> May 2020
-                        <b></b>
+                        <b>Currently Attending:</b> University of Guelph<br>
+                        <b>Expected Graduation:</b> May 2020<br>
                     </p>
                 </div>
             </div>
@@ -45,36 +44,109 @@ require_once (__DIR__ . "/../header.html");
     <div id="educationSection" class="container-fluid section education-section">
         <div class="content">
             <h1>Education</h1>
-            <p class="lead-in"> Education is not the learning of fact, but the training of the mind to think<br>- Albert Einstein</p>
+            <p class="lead-in">Education is not the learning of fact, but the training of the mind to think<br>- Albert Einstein</p>
             <hr>
             <div class="row">
-                <div class="col-md-4">
+                <div class="col-md-6">
+                    <h4>University of Guelph</h4>
                     <p>
-                        <b>University of Guelph</b>
-                        <br>
-                        Sept 2015 - May 2020
+                        Sept 2015 - May 2020<br>
+                        <i class="fas fa-map-marker-alt"></i> Guelph, ON<br>
                     </p>
                 </div>
-                <div class="col-md-6">
-                    <p><b>Bachelor of Computing - Major: Computer Science, Minor: Business</b></p>
+
+                <div class="col-md-6" style="text-align: left">
+                    <h4>Bachelor of Computing</h4>
+                    <p>
+                        <b>Major:</b> Computer Science - Co-op <br>
+                        <b>Minor:</b> Business<br>
+                        The University of Guelph's major in Computer Science provides a strong foundation in
+                        software development, designing solutions to complex problems and important theoretical concepts.
+                        The co-op program offers students the opportunity to work under a mentor at various companies to
+                        gain valuable industry experience.
+                    </p>
                 </div>
             </div>
         </div>
     </div>
     <div id="experienceSection" class="container-fluid section experience-section">
-        <h1>Section 3</h1>
-        <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+        <div class="content">
+            <h1>Experience</h1>
+            <p class="lead-in">
+                Experience is the teacher of all things<br>- Julius Caesar
+            </p>
+            <hr>
+
+            <div id="tulipExperience" class="row">
+                <div class="content">
+                    <div class="row">
+                        <div class="job-header">
+                            <h2 style="color: #F15F77; margin-left: auto; margin-top: auto;">
+                                <img src="/../../global/images/tulip_logo.svg"> - Full Stack Developer
+                            </h2>
+
+<!--                            <h2>Tulip Retail - Full Stack Developer</h2>-->
+                            <p class="align-left">
+                                May 2017 - Aug 2017
+                            </p>
+                            <p class="align-right">
+                                <i class="fas fa-map-marker-alt"></i> Toronto, ON
+                            </p>
+                        </div>
+<!--                        <img style="margin-left: auto; margin-bottom: auto;" src="/../../global/images/tulip_logo.svg">-->
+                    </div>
+                    <div class="row">
+                        <h4>About The Company</h4>
+                        <p style="text-align: left">
+                            Tulip Retail has an innovative product, a powerful cloud-based, mobile platform built to
+                            empower store associates. Tulip brings the best of online retail to brick-and-mortar stores
+                            by delivering a powerful application that enhances the in-store experience.
+
+
+                            The iOS app used by store associates is an extremely powerful tool to connect associates
+                            with customers in a brand new way. The idea of introducing omnichannel avenues into a
+                            brick-and-mortar store has been revolutionary thus far and continues to pick up momentum.
+                            Tulip has become a leader in arming retailers with important data and tools for success.
+                            Through this process Tulip has become an industry leader in the market space, with nobody
+                            else doing what Tulip is doing.
+
+
+                            Ali Asaria, CEO of Tulip Retail, first founded the company well.ca before moving on to found
+                            Tulip. Well.ca is an online health, beauty and pharmaceutical retailer based entirely in
+                            Canada. Ali was also the man behind the BrickBreaker game found on BlackBerry devices from
+                            his days working at Research in Motion. Ali started Tulip Retail in Waterloo, ON in 2013 and
+                            has quickly grown in size to almost 200 employees across Canada, the U.S. and Europe.
+                        </p>
+                    </div>
+
+
+<!--                    <div class="row">-->
+<!--                        <div class="col-lg-2 col">-->
+<!--                            <p>-->
+<!--                                May 2017 - Aug 2017<br>-->
+<!--                                <i class="fas fa-map-marker-alt"></i> Toronto, ON<br>-->
+<!--                            </p>-->
+<!--                        </div>-->
+<!---->
+<!--                        <div class="col-lg-10" style="text-align: left">-->
+<!--                            <p>-->
+<!--                                During my 4 month co-op at Tulip I worked on a team which serviced the client well.ca the team-->
+<!--                                consisted of myself, another co-op and our manager. The small team dynamic meant that we all had-->
+<!--                                to be able to deal with all components of the very large and intricate website.-->
+<!--                            </p>-->
+<!--                        </div>-->
+<!--                    </div>-->
+                </div>
+            </div>
+
+            <div id="brockExperience"></div>
+
+            <div id="revyoomeExperience"></div>
+
+            <div id="adknownExperience"></div>
+
+        </div>
     </div>
-<!--    <div id="section41" class="container-fluid">-->
-<!--        <h1>Section 4 Submenu 1</h1>-->
-<!--        <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>-->
-<!--        <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>-->
-<!--    </div>-->
-<!--    <div id="section42" class="container-fluid">-->
-<!--        <h1>Section 4 Submenu 2</h1>-->
-<!--        <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>-->
-<!--        <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>-->
-<!--    </div>-->
 
     </body>
     </html>
